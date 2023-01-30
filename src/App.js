1960d6d89fdc3579e62d9614feb2ee3fa680af9f
Megan Smith <megans766@gmail.com>
2023-01-30 17:19:26 -0500

    update routes

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='home' element={Home />} />
-        <Route path='about' element={About />} />
-        <Route path='contact' element={Contact />} />
-        <Route path='projects' element={Projects />} />
-        <Route path='resume' element={Resume />} />
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='contact' element={<Contact />} />
+        <Route path='projects' element={<Projects />} />
+        <Route path='resume' element={<Resume />} />
       </Routes>
     </>
   );
