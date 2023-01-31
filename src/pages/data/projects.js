e24278d0e0dbb99b76d84d534474e7020d45d552
Megan Smith <megans766@gmail.com>
2023-01-31 16:12:39 -0500

    import images & update project component

--- src/pages/data/projects.js
+++ src/pages/data/projects.js
@@ -4,14 +4,14 @@ const projects = [
     description: 'A relaxed beach redo on the classic connect 4 game.',
     repoLink: 'https://github.com/Megans766/connect-4',
     deploymentLink: 'https://rad-shortbread-da9ba9.netlify.app/',
-    image: <img src='../Connect4.png' alt='' />
+    image: <img src='../images/Connect4.png' alt='' />
   },
   {
     title: 'Thriller Tracker',
     description: 'A platform for amusement park enthusiasts. Users can share highlights of their adventures as well as create a bucket list of future adventures to go on.',
     repoLink: 'https://github.com/Megans766/Thriller-Tracker',
     deploymentLink: 'https://flyio-thriller-tracker.fly.dev/',
-    image: <img src='../Thriller-Tracker.png' alt='' />
+    image: <img src='../images/Thriller-Tracker.png' alt='' />
   }
 ]
 
