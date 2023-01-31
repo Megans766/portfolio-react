17a754cfe3bdd808fc03462ec32cb6628eda6e2b
Megan Smith <megans766@gmail.com>
2023-01-30 20:32:26 -0500

    update About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,18 @@ function About() {
     <>
       <h2>About Megan</h2>
       <h4>maybe put a photo here</h4>
+      <p>
+        I'm a Software Engineer with a background in renewable energy. I enjoy problem-solving, working on team oriented projects, and acquiring new skills and knowledge. I am currently pursuing a BS in Computer Engineering from SNHU and a Software Engineering Immersive Certificate from General Assembly.
+      </p>
+      <h3>Hobbies</h3>
+      <div>
+        <div>Escape Rooms</div>
+        <div>Assembling Lego Sets</div>
+        <div>Mobile Games</div>
+        <div>Reading</div>
+        <div>Mini Golf</div>
+      </div>
+
     </>
   )
 }
