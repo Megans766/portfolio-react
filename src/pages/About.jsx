783f883c7610d8308ffb93f60348d8d62242fd79
Megan Smith <megans766@gmail.com>
2023-01-31 14:47:12 -0500

    update resume component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ function About() {
       <h2>About Megan</h2>
       <h4>maybe put a photo here</h4>
       <p>
-        I'm a Software Engineer with a background in renewable energy. I enjoy problem-solving, working on team oriented projects, and acquiring new skills and knowledge. I am currently pursuing a BS in Computer Engineering from SNHU and a Software Engineering Immersive Certificate from General Assembly.
+        I'm a Software Engineer with a background in renewable energy. I enjoy problem-solving, working on team oriented projects, and acquiring new skills and knowledge. I am currently pursuing a BS in Computer Science from SNHU and a Software Engineering Immersive Certificate from General Assembly.
       </p>
       <h3>Hobbies</h3>
       <div>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,6 +1,21 @@
 function Resume() {
   return (
+    <>
     <h1>Resume</h1>
+    <p>Click <a href='./Resume.pdf'>here </a>to download my resume</p>
+    <h3>Current Skills</h3>
+    <div>
+      <p>HTML</p>
+      <p>CSS</p>
+      <p>JavaScript</p>
+      <p>Express</p>
+      <p>MongoDB</p>
+      <p>Mongoose</p>
+      <p>EJS</p>
+      <p>React</p>
+      <p>Node.js</p>
+    </div>
+    </>
   )
 }
 
