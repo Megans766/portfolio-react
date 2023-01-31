9b7b1b23dd0cf6a8987938c899a115c5bca30382
Megan Smith <megans766@gmail.com>
2023-01-30 20:46:28 -0500

    update contact component

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,11 @@
 function Contact() {
   return (
-    <h1>Contact</h1>
+    <>
+      <h1>Contact Me</h1>
+      <p>Email: Megans766@gmail.com</p>
+      <p>LinkedIn: https://www.linkedin.com/in/megansmith2123/</p>
+      <p>GitHub: https://github.com/Megans766</p>
+    </>
   )
 }
 
